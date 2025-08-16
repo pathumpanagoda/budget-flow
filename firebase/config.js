ae2c2d80e34f25aa7eaefeb3cd9cece72260dd62
Dilshan Pathum <128419282+pathumpanagoda@users.noreply.github.com>
2025-08-16 22:28:25 +0530

    Update config.js
    update firebase config

--- firebase/config.js
+++ firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_Nuw1MOkNnvJlKtwpbebqV2U-Au2vnW4",
-  authDomain: "event-app-91310.firebaseapp.com",
-  projectId: "event-app-91310",
-  storageBucket: "event-app-91310.firebasestorage.app",
-  messagingSenderId: "1025959339738",
-  appId: "1:1025959339738:web:f2aafcfc3ed0cd6542a7e3"
+  apiKey: "test",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
